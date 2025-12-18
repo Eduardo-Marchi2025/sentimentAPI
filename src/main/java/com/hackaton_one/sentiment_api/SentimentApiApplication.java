f0b54c6979e91670fe89b73b216425b1485fb367
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-12-18 16:32:47 -0300

    feat: add swagger support

--- src/main/java/com/hackaton_one/sentiment_api/SentimentApiApplication.java
+++ src/main/java/com/hackaton_one/sentiment_api/SentimentApiApplication.java
@@ -1,8 +1,16 @@
 package com.hackaton_one.sentiment_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Sentiment Analysis API",
+                version = "1.0",
+                description = "API para análise de sentimento de textos."
+        )
+)
 @SpringBootApplication
 public class SentimentApiApplication {
 
